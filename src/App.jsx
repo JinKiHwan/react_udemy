097a3bbd5707652c0b679b45601a5e1e5c45171e
Kihwan <eljsh95@naver.com>
2025-02-21 14:49:13 +0900

    컨텐츠 영역 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,10 @@ import { EXAMPLES } from '../data.js';
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import Box from './components/Box.jsx';
 
 function App() {
-    const [selectedTopic, setSelectedTopic] = useState();
-
-    function handleSelect(selectedButton) {
-        setSelectedTopic(selectedButton);
-        console.log(selectedTopic);
-    }
-
     console.log('APP UPDATE!');
-
-    let tabContent = <p>Please select a topic.</p>;
-    if (selectedTopic) {
-        tabContent = (
-            <div id="tab-content">
-                <h3>{EXAMPLES[selectedTopic].title}</h3>
-                <p>{EXAMPLES[selectedTopic].description}</p>
-                <pre>
-                    <code>{EXAMPLES[selectedTopic].code}</code>
-                </pre>
-            </div>
-        );
-    }
 
     return (
         <div>
@@ -44,17 +25,7 @@ function App() {
 
                 <section id="examples">
                     <h2>Examples</h2>
-                    <menu>
-                        <TabButton
-                            isSelected={selectedTopic === 'components'}
-                            onSelect={() => handleSelect('components')}>
-                            Components
-                        </TabButton>
-                        <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-                        <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-                        <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
-                    </menu>
-                    {tabContent}
+                    <Box></Box>
                 </section>
             </main>
         </div>
