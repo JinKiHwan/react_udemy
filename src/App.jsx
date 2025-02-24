63c7203cd14806724a31f7d2561fab18b34a920d
Kihwan <eljsh95@naver.com>
2025-02-24 18:43:59 +0900

    플레이어 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Player from './components/Player';
+
 function App() {
-    return <p>Coming soon...</p>;
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <Player name="player 1" symbol="X"></Player>
+                    <Player name="player 2" symbol="O"></Player>
+                </ol>
+                GAMEBOAR
+            </div>
+        </main>
+    );
 }
 
 export default App;
