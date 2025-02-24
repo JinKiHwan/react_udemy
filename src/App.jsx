1c24e1fed3484e571e8ec3f219ba966891dcb358
Jin KiHwan <eljsh95@naver.com>
2025-02-24 23:14:45 +0900

    edit버튼 기능 1차 추 가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Player from './components/Player';
 
 function App() {
-    return (
-        <main>
-            <div id="game-container">
-                <ol id="players">
-                    <Player name="player 1" symbol="X"></Player>
-                    <Player name="player 2" symbol="O"></Player>
-                </ol>
-                GAMEBOAR
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="player 1" symbol="X"></Player>
+          <Player name="player 2" symbol="O"></Player>
+        </ol>
+        GAMEBOAR
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+
 export default function Player({ name, symbol }) {
-    return (
-        <li>
-            <span className="player">
-                <span className="player-name">{name}</span>
-                <span className="player-symbol">{symbol}</span>
-            </span>
-            <button>Edit</button>
-        </li>
-    );
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let btnCaption = 'Edit';
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+    btnCaption = 'Save';
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {/* {isEditing == false ? <span className="player-name">{name}</span> : <span className="player-name">null</span>}*/}
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{btnCaption}</button>
+    </li>
+  );
 }
