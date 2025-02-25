57a06786d310d8b6b44961634f2fd5aef387224d
Kihwan <eljsh95@naver.com>
2025-02-25 18:23:07 +0900

    플레이어 교차 셀렉

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import Player from './components/Player';
 import Gamebord from './components/Gameboard';
+import { useState } from 'react';
 
 function App() {
+    const [activePlayer, setActivePlayer] = useState('X');
+
+    function handleSelectSquare() {
+        setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+    }
     return (
         <main>
             <div id="game-container">
-                <ol id="players">
-                    <Player initialName="player 1" symbol="X"></Player>
-                    <Player initialName="player 2" symbol="O"></Player>
+                <ol id="players" className="highlight-player">
+                    <Player initialName="player 1" symbol="X" isActive={activePlayer === 'X'}></Player>
+                    <Player initialName="player 2" symbol="O" isActive={activePlayer === 'O'}></Player>
                 </ol>
 
-                <Gamebord></Gamebord>
+                <Gamebord onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}></Gamebord>
             </div>
         </main>
     );
